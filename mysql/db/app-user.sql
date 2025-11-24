1378a3aad84518681231e21c4d22f90180c13f73
Poguri Diya <diya>
2025-11-24 16:27:36 +0530

    docker

--- mysql/db/app-user.sql
+++ mysql/db/app-user.sql
@@ -1,3 +1,3 @@
-CREATE USER IF NOT EXISTS 'shipping'@'%' IDENTIFIED WITH mysql_native_password BY 'RoboShop@1';
-GRANT ALL ON cities.* TO 'shipping'@'%';
-FLUSH PRIVILEGES;
+version https://git-lfs.github.com/spec/v1
+oid sha256:86dd1418dfcfbaed252fef79d56abb0a42c1c554d5777797b57a2bbf5dde4691
+size 156
